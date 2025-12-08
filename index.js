493a29608c0b3f31ae47c741634ebf43b79757a4
Chema Piquero <chpiquero@gmail.com>
2025-12-08 18:14:53 +0100

    feat: Implement form submission handling with Formspree and add email input field

--- index.js
+++ index.js
@@ -187,3 +187,35 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+// Formspree form handler
+var form = document.getElementById("my-form");
+
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("my-form-status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+    if (response.ok) {
+      status.innerHTML = "¡Gracias por tu mensaje! Nos pondremos en contacto pronto.";
+      form.reset()
+    } else {
+      response.json().then(data => {
+        if (Object.hasOwn(data, 'errors')) {
+          status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+        } else {
+          status.innerHTML = "¡Oops! Hubo un problema al enviar tu formulario"
+        }
+      })
+    }
+  }).catch(error => {
+    status.innerHTML = "¡Oops! Hubo un problema al enviar tu formulario"
+  });
+}
+form.addEventListener("submit", handleSubmit)
